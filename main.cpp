1e1015a60cfe5516359cef6912d1fcfb21734967
ZetZhang <13660591402@163.com>
2022-08-24 03:09:30 +0800

    profit

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 
 int main(int argc, char *argv[])
 {
-    wsv::WebServer server(1316, 3, 60000, false, 3306, "root", "zjt152445", "yourdb", 12, 6, true, 1, 1024);
+    wsv::WebServer server(12309, 3, 60000, false, 3306, "root", "zjt152445", "yourdb", 12, 6, true, 1, 1024);
     server.start();
 }
